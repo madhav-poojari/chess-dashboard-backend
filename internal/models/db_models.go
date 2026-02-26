6e836eafcc018c2df6791286ecf0a54ba712c3c7
kushagra2207 <fantastick22072006@gmail.com>
2026-02-27 02:57:43 +0530

    feat: add academy gallery api and update col_name in images table

--- internal/models/db_models.go
+++ internal/models/db_models.go
@@ -129,7 +129,7 @@ type Image struct {
 	URLSuffix            string    `gorm:"column:url_suffix;not null" json:"url_suffix"`
 	Filename             string    `gorm:"not null" json:"filename"`
 	Title                string    `gorm:"not null" json:"title"`
-	PositionInTournament string    `gorm:"column:position_in_tournament" json:"position_in_tournament"`
+	Tags                 datatypes.JSON `gorm:"type:jsonb;default:'[]'" json:"tags"`
 	IsPrivate            bool      `gorm:"default:false" json:"is_private"`
 	CreatedAt            time.Time `json:"created_at"`
 }
